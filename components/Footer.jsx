8e15dfcfb953234dc291401f3606327e71b8603b
RenTung <winterdong526@gmail.com>
2022-09-22 14:38:09 +0800

    上icon串連結

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <p>2022 Una's Clay Work All rights reserverd</p>
       <p className='icons'>
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href='https://www.instagram.com/unaclaywork/'>
+          <AiFillInstagram />
+        </a>
+        <a href='https://www.facebook.com/unaclaywork/'>
+          <AiFillFacebook />
+        </a>
+        <a href='https://shopee.tw/unanannie0609'>
+          <FaShoppingCart />
+        </a>
       </p>
     </div>
   )
